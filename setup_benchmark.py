54bac8d5e6fea4b9a58c76aa53faeac77bf35618
Peiyi Li <pli11@vclvm177-12.vcl.ncsu.edu>
2021-11-10 02:09:10 -0500

    add setup file to setup two package

---
+++ setup_benchmark.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+#generate benchmark package  
+setup(name='benchmark',
+      packages=['benchmark' ],
+      )
---
+++ setup_hamap.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+#this hamap package is from https://github.com/peachnuts/HA
+setup(name='hamap',
+      packages=['hamap' ],
+      )
