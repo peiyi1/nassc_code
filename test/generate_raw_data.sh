acae6c14bea59bb1ec844ead23542b875f4bbc49
Peiyi Li <pli11@vclvm177-12.vcl.ncsu.edu>
2021-11-10 03:22:23 -0500

    add result folder and scripts to generate the results

---
+++ test/generate_raw_data.sh
@@ -0,0 +1,88 @@
+cd test_CouplingMap_FullyConnected
+python run_benchmark.py ../yaml_file/mod5d2_64.yaml
+python run_benchmark.py ../yaml_file/mod5mils_65.yaml
+python run_benchmark.py ../yaml_file/3_17_13.yaml
+python run_benchmark.py ../yaml_file/decod24-v2_43.yaml
+python run_benchmark.py ../yaml_file/bv_n5.yaml
+python run_benchmark.py ../yaml_file/grover_n4.yaml
+python run_benchmark.py ../yaml_file/grover_n6.yaml
+python run_benchmark.py ../yaml_file/grover_n8.yaml
+python run_benchmark.py ../yaml_file/vqe_n8.yaml
+python run_benchmark.py ../yaml_file/vqe_n12.yaml
+python run_benchmark.py ../yaml_file/bv_n19.yaml
+python run_benchmark.py ../yaml_file/qft_n15.yaml
+python run_benchmark.py ../yaml_file/qft_n20.yaml
+python run_benchmark.py ../yaml_file/qpe_n9.yaml
+python run_benchmark.py ../yaml_file/adder_n10.yaml
+python run_benchmark.py ../yaml_file/multiplier_n25.yaml
+python run_benchmark.py ../yaml_file/sqn_258.yaml
+python run_benchmark.py ../yaml_file/rd84_253.yaml
+python run_benchmark.py ../yaml_file/co14_215.yaml
+python run_benchmark.py ../yaml_file/sym9_193.yaml
+
+cd ../test_CouplingMap_montreal
+python run_benchmark.py ../yaml_file/mod5d2_64.yaml
+python run_benchmark.py ../yaml_file/mod5mils_65.yaml
+python run_benchmark.py ../yaml_file/3_17_13.yaml
+python run_benchmark.py ../yaml_file/decod24-v2_43.yaml
+python run_benchmark.py ../yaml_file/bv_n5.yaml
+python run_benchmark.py ../yaml_file/grover_n4.yaml
+python run_benchmark.py ../yaml_file/grover_n6.yaml
+python run_benchmark.py ../yaml_file/grover_n8.yaml
+python run_benchmark.py ../yaml_file/vqe_n8.yaml
+python run_benchmark.py ../yaml_file/vqe_n12.yaml
+python run_benchmark.py ../yaml_file/bv_n19.yaml
+python run_benchmark.py ../yaml_file/qft_n15.yaml
+python run_benchmark.py ../yaml_file/qft_n20.yaml
+python run_benchmark.py ../yaml_file/qpe_n9.yaml
+python run_benchmark.py ../yaml_file/adder_n10.yaml
+python run_benchmark.py ../yaml_file/multiplier_n25.yaml
+python run_benchmark.py ../yaml_file/sqn_258.yaml
+python run_benchmark.py ../yaml_file/rd84_253.yaml
+python run_benchmark.py ../yaml_file/co14_215.yaml
+python run_benchmark.py ../yaml_file/sym9_193.yaml
+
+cd ../test_CouplingMap_linear
+python run_benchmark.py ../yaml_file/mod5d2_64.yaml
+python run_benchmark.py ../yaml_file/mod5mils_65.yaml
+python run_benchmark.py ../yaml_file/3_17_13.yaml
+python run_benchmark.py ../yaml_file/decod24-v2_43.yaml
+python run_benchmark.py ../yaml_file/bv_n5.yaml
+python run_benchmark.py ../yaml_file/grover_n4.yaml
+python run_benchmark.py ../yaml_file/grover_n6.yaml
+python run_benchmark.py ../yaml_file/grover_n8.yaml
+python run_benchmark.py ../yaml_file/vqe_n8.yaml
+python run_benchmark.py ../yaml_file/vqe_n12.yaml
+python run_benchmark.py ../yaml_file/bv_n19.yaml
+python run_benchmark.py ../yaml_file/qft_n15.yaml
+python run_benchmark.py ../yaml_file/qft_n20.yaml
+python run_benchmark.py ../yaml_file/qpe_n9.yaml
+python run_benchmark.py ../yaml_file/adder_n10.yaml
+python run_benchmark.py ../yaml_file/multiplier_n25.yaml
+python run_benchmark.py ../yaml_file/sqn_258.yaml
+python run_benchmark.py ../yaml_file/rd84_253.yaml
+python run_benchmark.py ../yaml_file/co14_215.yaml
+python run_benchmark.py ../yaml_file/sym9_193.yaml
+
+cd ../test_CouplingMap_grid
+python run_benchmark.py ../yaml_file/mod5d2_64.yaml
+python run_benchmark.py ../yaml_file/mod5mils_65.yaml
+python run_benchmark.py ../yaml_file/3_17_13.yaml
+python run_benchmark.py ../yaml_file/decod24-v2_43.yaml
+python run_benchmark.py ../yaml_file/bv_n5.yaml
+python run_benchmark.py ../yaml_file/grover_n4.yaml
+python run_benchmark.py ../yaml_file/grover_n6.yaml
+python run_benchmark.py ../yaml_file/grover_n8.yaml
+python run_benchmark.py ../yaml_file/vqe_n8.yaml
+python run_benchmark.py ../yaml_file/vqe_n12.yaml
+python run_benchmark.py ../yaml_file/bv_n19.yaml
+python run_benchmark.py ../yaml_file/qft_n15.yaml
+python run_benchmark.py ../yaml_file/qft_n20.yaml
+python run_benchmark.py ../yaml_file/qpe_n9.yaml
+python run_benchmark.py ../yaml_file/adder_n10.yaml
+python run_benchmark.py ../yaml_file/multiplier_n25.yaml
+python run_benchmark.py ../yaml_file/sqn_258.yaml
+python run_benchmark.py ../yaml_file/rd84_253.yaml
+python run_benchmark.py ../yaml_file/co14_215.yaml
+python run_benchmark.py ../yaml_file/sym9_193.yaml
+
